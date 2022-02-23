f6cd1704c66c83a39fa9c0719c387021a7d0fad0
sanckh <csutton@student.fullsail.edu>
2022-02-22 21:29:03 -0600

    initial commit

---
+++ code.js
@@ -0,0 +1,16 @@
+const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let moves = 0;
+
+
+
+    const playGame = () => {
+        const rockBtn = document.querySelector('.rock');
+        
+    }
+
+}
+
+
+
