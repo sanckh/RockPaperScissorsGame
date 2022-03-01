bea2b5d236a99b3da346251762b4adc12c854b8e
sanckh <csutton@student.fullsail.edu>
2022-03-01 11:40:28 -0600

    finished

--- code.js
+++ code.js
@@ -2,38 +2,39 @@ const game = () => {
     let playerScore = 0;
     let computerScore = 0;
     let moves = 0;
-
-
-
+ 
+ 
     const playGame = () => {
         const rockBtn = document.querySelector('.rock');
         const paperBtn = document.querySelector('.paper');
         const scissorBtn = document.querySelector('.scissor');
         const playerOptions = [rockBtn,paperBtn,scissorBtn];
         const computerOptions = ['rock','paper','scissors']
+         
 
-        // Function to start playing
         playerOptions.forEach(option => {
             option.addEventListener('click',function(){
+ 
                 const movesLeft = document.querySelector('.movesleft');
                 moves++;
                 movesLeft.innerText = `Moves Left: ${5-moves}`;
-
-
+                 
+ 
                 const choiceNumber = Math.floor(Math.random()*3);
                 const computerChoice = computerOptions[choiceNumber];
-                
+ 
+
                 winner(this.innerText,computerChoice)
-                
+                 
+
                 if(moves == 5){
                     gameOver(playerOptions,movesLeft);
                 }
             })
         })
-
+         
     }
-
-    // Function to decide winner
+ 
     const winner = (player,computer) => {
         const result = document.querySelector('.result');
         const playerScoreBoard = document.querySelector('.p-count');
@@ -48,7 +49,7 @@ const game = () => {
                 result.textContent = 'Computer Won';
                 computerScore++;
                 computerScoreBoard.textContent = computerScore;
-
+ 
             }else{
                 result.textContent = 'Player Won'
                 playerScore++;
@@ -78,34 +79,34 @@ const game = () => {
             }
         }
     }
-
+ 
     const gameOver = (playerOptions,movesLeft) => {
-
+ 
         const chooseMove = document.querySelector('.move');
         const result = document.querySelector('.result');
         const reloadBtn = document.querySelector('.reload');
-
+ 
         playerOptions.forEach(option => {
             option.style.display = 'none';
         })
-
-
-        chooseMove.innerText = 'Game Over'
+ 
+      
+        chooseMove.innerText = 'Game Over!!'
         movesLeft.style.display = 'none';
-
+ 
         if(playerScore > computerScore){
             result.style.fontSize = '2rem';
-            result.innerText = 'Player Won'
+            result.innerText = 'You Won The Game'
             result.style.color = '#308D46';
         }
         else if(playerScore < computerScore){
             result.style.fontSize = '2rem';
-            result.innerText = 'Computer Won';
+            result.innerText = 'You Lost The Game';
             result.style.color = 'red';
         }
         else{
             result.style.fontSize = '2rem';
-            result.innerText = 'Tie Game';
+            result.innerText = 'Tie';
             result.style.color = 'grey'
         }
         reloadBtn.innerText = 'Restart';
@@ -114,9 +115,9 @@ const game = () => {
             window.location.reload();
         })
     }
-
+ 
     playGame();
-
+     
 }
 
 game();
